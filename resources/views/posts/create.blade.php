4c9c5edafa57a2c531bd15ef8bf49fcfc3a0901a
tRQ0 <rq1087@gmail.com>
2023-04-08 12:52:43 +0530

    add cover image field

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,18 +1,30 @@
 @extends('layouts/default')
 
 @section('content')
-<div class="col">
-    <h1>Create Post</h1>
-    <hr>
-    <form method='POST' action='{{route('post.store')}}'>
-        <div class='form-group'>
-            @csrf
-            <label for='title' class='form-label'>Title</label>
-            <input type='text' id='title' name='title' placeholder='Post title' class="form-control @error('body') is-invalid @enderror"/>
-            <label for='body' class='form-label'>Body</label>
-            <textarea type='text' id='body' name='body' placeholder='Post body' class="form-control @error('body') is-invalid @enderror"></textarea>
-            <input type='submit' value='Create Post' class='btn btn-primary'><br>
-        </div>        
-    </form>
-</div>
+    <div class="col">
+        <h1>Create Post</h1>
+        <hr>
+        <div class="card">
+            <div class="card-body">
+                <form method="POST" action="{{ route('post.store') }}" enctype="multipart/form-data">
+                    <div class="form-group">
+                        @csrf
+                        <label for='title' class='form-label'>Title</label>
+                        <input type='text' id='title' name='title' placeholder='Post title'
+                            class="form-control @error('body') is-invalid @enderror" />
+                        <label for='body' class='form-label'>Body</label>
+                        <textarea type='text' id='body' name='body' placeholder='Post body'
+                            class="form-control @error('body') is-invalid @enderror"></textarea>
+                        <div class="form-group">
+                            <input type="file" class="btn btn-primary mt-2" name="coverImage" />
+                        </div>
+                    </div>
+            </div>
+            <div class="card-footer">
+                <input type='submit' value='Create Post' class='btn btn-primary'><br>
+            </div>
+            </form>
+        </div>
+
+    </div>
 @endsection
