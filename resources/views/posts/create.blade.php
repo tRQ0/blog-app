595b46a6c5df253b48060e8a9779099049513773
tRQ0 <rq1087@gmail.com>
2023-04-08 19:58:57 +0530

    fix layout

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,8 +15,9 @@
                         <label for='body' class='form-label'>Body</label>
                         <textarea type='text' id='body' name='body' placeholder='Post body'
                             class="form-control @error('body') is-invalid @enderror"></textarea>
+                        <label for='coverImage' class='form-label pt-3'>Cover Image</label>
                         <div class="form-group">
-                            <input type="file" class="btn btn-primary mt-2" name="coverImage" />
+                            <input type="file" class="btn btn-primary" id="coverImage" name="coverImage" />
                         </div>
                     </div>
             </div>
