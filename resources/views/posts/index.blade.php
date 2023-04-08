39c039075463210afbfde732bfa68ab391b0aee6
tRQ0 <rq1087@gmail.com>
2023-04-08 12:50:50 +0530

    add cover image

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,17 +4,35 @@
     <div class="col">
         @foreach ($posts as $post)
             <div class="card my-3">
-                <div class="card-header">
-
-                </div>
                 <div class="card-body">
-                    <h1><a href='{{ route('post.show', $post->id) }}'><u>{!! $post->title !!}</u></a></h1>
+                    {{-- load when cover image exist --}}
+                    @if ($post->cover_image)
+                        <div class="row">
+                            <div class="col-md-4 col-sm-4" style="height: 50%;">
+
+                                <img style="width: 100%; object-fit: contain; object-position: 100% 0;"
+                                    src="storage/cover_images/{{ $post->cover_image }}">
+
+
+                            </div>
+                            <div class="col-md-8 col-sm-4">
+                                <h1>
+                                    <a href="{{ route('post.show', $post->id) }}"><u>{!! $post->title !!}</u></a>
+                                </h1>
+                            </div>
+                        </div>
+                    @else
+                        {{-- load when cover image does not exist --}}
+                        <h1><a href="{{ route('post.show', $post->id) }}"><u>{!! $post->title !!}</u></a></h1>
+                    @endif
                     <hr>
+
                     Author: {{ $post->user->name }}
                     <div class="data float-end">
                         Created - {{ $post->created_at->diffForHumans() }}
                     </div>
                 </div>
+
             </div>
         @endforeach
         <div class="row row-outline">
